88f633efe998b5f452a695179452c905643b5428
absimler <39098110+absimler@users.noreply.github.com>
2024-02-05 09:42:12 -0700

    Add files via upload

---
+++ hw3_answers.R
@@ -0,0 +1,43 @@
+#Homework 3 answers #
+# Due Feb 11, 2020 #
+
+
+# Please look at the github repository instructions for full question prompts:
+
+
+## Question 1: Probability of independent events
+
+
+#1a) 
+
+
+#1b) 
+
+
+## Question 2: Responses of insectivorous bats to urbanization?
+
+#2a) 
+
+#2b) 
+
+#2c)
+
+# 2d)
+                                                                                          
+
+
+## Question 3: Simulating binomial data
+
+# 3a)
+
+# 3b)
+
+
+# 3c) 
+
+
+# 3d)
+
+
+# 3e)
+                                                                                        
